2f6415aa240cfd9853b3d44741158915e952726e
Shrikant Vishwakarma <64365738+StrixV54@users.noreply.github.com>
2023-08-24 23:46:51 +0530

    added codebox

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,10 @@ function Header() {
   return (
     <div className="topbar">
       <div className="logo">Logo</div>
+      <div className="route-select">
+        <button className="mapper-btn">Json Mapper</button>
+        <button className="jsonstring-btn">Json to String</button>
+      </div>
       <div className="switch">DarkMode</div>
     </div>
   );
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,44 +1,65 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+// import CodeFlask from "./CodeFlask";
+import CodeFlask from "codeflask";
 
 function Section() {
   const [codeValue, setCodeValue] = useState("");
   const [undoText, setUndoText] = useState("");
-  const [primaryText, setPrimaryText] = useState("");
+  const [primaryText, setPrimaryText] = useState(" ");
   const [secondaryText, setSecondaryText] = useState([]);
 
+  // if (newProps.code !== this.codeFlask.getCode()) {
+  //   this.codeFlask.updateCode(newProps.code);
+  // }
+  // console.log(primaryText);
+
+  useEffect(() => {
+    console.log("first");
+    const flask = new CodeFlask("#codebox", {
+      language: "js",
+      lineNumbers: true,
+      handleTabs: true,
+      defaultTheme: false,
+    });
+    flask.updateCode(primaryText);
+    // flask.updateCode(primaryText);
+    flask.onUpdate((code) => setPrimaryText(code));
+  }, []);
+
   const mapdata = () => {
-    let data = primaryText;
-    data !== undoText && setUndoText(data);
-    try {
-      let map = JSON.parse(secondaryText);
-      // let map = secondaryText;
-      // data = JSON.stringify(data);
-      console.log(data, " ", map);
-      // map.map((item) => console.log(item));
-      Object.keys(map).forEach((item) => {
-        console.log(item);
-        data = data.includes(item) ? data.replace(item, map[item]) : data;
-      });
-      console.log("---", data, " ", map);
-      setPrimaryText(data);
-    } catch (err) {
-      console.log(
-        err.toString().includes(`Expected property name or '}'`)
-          ? "Json Error"
-          : err
-      );
-    }
+    // let data = primaryText;
+    // data !== undoText && setUndoText(data);
+    // try {
+    //   let map = JSON.parse(secondaryText);
+    //   // let map = secondaryText;
+    //   // data = JSON.stringify(data);
+    //   console.log(data, " ", map);
+    //   // map.map((item) => console.log(item));
+    //   Object.keys(map).forEach((item) => {
+    //     console.log(item);
+    //     data = data.includes(item) ? data.replace(item, map[item]) : data;
+    //   });
+    //   console.log("---", data, " ", map);
+    //   setPrimaryText(data);
+    // } catch (err) {
+    //   console.log(
+    //     err.toString().includes(`Expected property name or '}'`)
+    //       ? "Json Error"
+    //       : err
+    //   );
+    //   }
   };
 
   return (
     <div className="section">
-      <div className="input-textbox">
+      <div id="codebox" />
+      {/* <div className="input-textbox">
         <textarea
           className="text-area"
           value={primaryText}
           onChange={(e) => setPrimaryText(e.target.value)}
         ></textarea>
-      </div>
+      </div> */}
       <div className="sidebar">
         <textarea
           className="text-area"
