cbfec9fef543eadc675ad64ad16bd6c87af14290
Shrikant Vishwakarma <64365738+StrixV54@users.noreply.github.com>
2023-08-31 00:48:51 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@
 import { useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Section from "./components/JsonMapper";
 import { Outlet } from "react-router-dom";
 
 function App() {
